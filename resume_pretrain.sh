5893a59df96f82a9e9fa32415a0bf20a93dc6dc6
yinghua_zhou@brown.edu <yzhou392@vscode1.oscar.ccv.brown.edu>
2025-09-28 02:41:53 -0400

    update default training param

--- resume_pretrain.sh
+++ resume_pretrain.sh
@@ -5,7 +5,7 @@ python -m torch.distributed.launch --nproc_per_node=2 main_pretrain.py \
   --model mae_vit_large_patch16 \
   --norm_pix_loss \
   --mask_ratio 0.75 \
-  --epochs 300 \
+  --epochs 400 \
   --warmup_epochs 20 \
   --blr 1.5e-4 \
   --weight_decay 0.05 \
