22be8107db7cb1f4b714ad6b6f14ad8e898d35cf
zyinghua <49605250+zyinghua@users.noreply.github.com>
2025-09-29 04:17:46 -0400

    set global_pool to false for vit pretrain

--- vit-pretrain.sh
+++ vit-pretrain.sh
@@ -11,6 +11,7 @@ python -m torch.distributed.launch --nproc_per_node=4 main_finetune.py \
   --weight_decay 0.1 \
   --drop_path 0.1 \
   --warmup_epochs 10 \
+  --global_pool False \
   --smoothing 0.1 \
   --mixup 0.5 \
   --cutmix 1.0 \
